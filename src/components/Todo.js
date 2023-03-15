9917ea96045af1fb52ac63f5a39b51578b033dba
John U <arjorb@gmail.com>
2023-03-15 17:37:23 +0200

    done creating a function that handle the changes of the input

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,22 @@
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import { HiPlusCircle } from 'react-icons/hi';
 
 import TodoList from './TodoList';
 const Todo = () => {
   const [data, setDate] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleChange = event => {
+    setInput(event.target.value);
+  };
+
+  console.log(input);
   return (
     <div className='w-[600px]'>
       <h1 className='text-6xl text-center font-bold text-gray-100'>Todos</h1>
       <div className='flex gap-3 items-center py-3 px-7 rounded-full shadow-3xl mt-5'>
-        <input type='text' placeholder='add todo...' className='w-full bg-transparent text-lg border-none outline-none placeholder:text-gray-200' />
+        <input type='text' placeholder='add todo...' onChange={handleChange} value={input} className='w-full bg-transparent text-lg border-none outline-none placeholder:text-gray-200' />
         <HiPlusCircle size={30} className='cursor-pointer text-green-500' />
       </div>
 
