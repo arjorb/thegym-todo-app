feb17c0b6e48b8eba74368841c550ac8eb98efce
John U <arjorb@gmail.com>
2023-03-15 19:00:47 +0200

    done working looping the data from the data state and get props to the todoList component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,22 +5,32 @@ import { HiPlusCircle } from 'react-icons/hi';
 import TodoList from './TodoList';
 const Todo = () => {
   const [data, setDate] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState();
 
   const handleChange = event => {
     setInput(event.target.value);
   };
 
-  console.log(input);
+  const handleAdd = () => {
+    const newData = {
+      id: nanoid(),
+      title: input,
+      isCheked: false,
+    };
+    setDate(prevData => [...prevData, newData]);
+    setInput('');
+  };
+
+  const todoElement = data.map(todo => <TodoList key={todo.id} {...todo} />);
   return (
     <div className='w-[600px]'>
       <h1 className='text-6xl text-center font-bold text-gray-100'>Todos</h1>
       <div className='flex gap-3 items-center py-3 px-7 rounded-full shadow-3xl mt-5'>
         <input type='text' placeholder='add todo...' onChange={handleChange} value={input} className='w-full bg-transparent text-lg border-none outline-none placeholder:text-gray-200' />
-        <HiPlusCircle size={30} className='cursor-pointer text-green-500' />
+        <HiPlusCircle onClick={handleAdd} size={30} className='cursor-pointer text-green-500' />
       </div>
-
-      <TodoList />
+      {data.length === 0 && <h1 className='text-4xl font-bold text-gray-200 text-center mt-20'>No Todo</h1>}
+      {todoElement}
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,11 @@
 import { FaTrash } from 'react-icons/fa';
-const TodoList = () => {
+const TodoList = ({ title }) => {
   return (
     <div className='flex justify-between gap-5 items-center mt-5 border-b-2 border-gray-100 pb-2'>
       <div className='flex gap-3 items-baseline'>
         <input type='checkbox' id='todo' />
-        <label htmlFor='todo' className='text-lg text-gray-500 leading-tight'>
-          A To do app developed using to consolidate your knowledge about react
+        <label htmlFor='todo' className='text-xl text-gray-500 leading-tight'>
+          {title}
         </label>
       </div>
       <div className='bg-red-100 p-3 rounded-full cursor-pointer'>
